b54cba221cd958c04d6ab4693de2ee3e38d2aca5
Olivier_pc_Portable <olivier.evrard77@gmail.com>
2024-07-29 20:12:33 +0200

    Version 29.07.2

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
 import streamlit as st
 
+def main():
+    st.set_page_config(page_title="Tinder", page_icon=":couple:", layout="centered", initial_sidebar_state="auto")
+    st.write("# Projet Tinder")
 
+    st.logo("icons/tinder-logo.png")
+    preambule = st.Page("pages/preambule.py", title="Projet", icon=":material/engineering:", default=True)
+    donnees = st.Page("pages/donnees.py", title="Forme des données fournies", icon=":material/database:")
+    physique = st.Page("pages/physique.py", title="Physique", icon=":material/directions_run:")
 
-st.set_page_config(page_title="Tinder", page_icon=":couple:", layout="centered", initial_sidebar_state="auto")
-st.write("# Projet Tinder")
+    origines = st.Page("pages/origines.py", title="Origine", icon=":material/diversity_3:")
+    age = st.Page("pages/age.py", title="Age", icon=":material/person:")
+    social = st.Page("pages/social.py", title="Profil social", icon=":material/language:")
 
-st.logo("icons/tinder-logo.png")
-preambule = st.Page("pages/preambule.py", title="Projet", icon=":material/engineering:", default=True)
-donnees = st.Page("pages/donnees.py", title="Forme des données fournies", icon=":material/database:")
-physique = st.Page("pages/physique.py", title="Physique", icon=":material/directions_run:")
+    pages = {
+        "🏠 Préambule": [preambule,],
+        "📊 Données fournies": [donnees,],
+        "🌍 Profil des participants": [physique, origines, age, social],
+        "📈 Attentes des participants": [],
+    }
 
-origines = st.Page("pages/origines.py", title="Origine", icon=":material/diversity_3:")
-age = st.Page("pages/age.py", title="Age", icon=":material/person:")
-social = st.Page("pages/social.py", title="Profil social", icon=":material/language:")
+    pg = st.navigation(pages)
+    pg.run()
 
-pages = {
-    "🏠 Préambule": [preambule,],
-    "📊 Données fournies": [donnees,],
-    "🌍 Profil des participants": [physique, origines, age, social],
-    "📈 Attentes des participants": [],
-}
-
-pg = st.navigation(pages)
-pg.run()
-
+if __name__=='__main__':
+    main()
--- pages/donnees.py
+++ pages/donnees.py
@@ -1,6 +1,18 @@
 import streamlit as st
 import pandas as pd
+from function import load_data
 
-df = pd.read_csv("Speed_Dating_Data.csv", encoding="cp1252")
+# df = pd.read_csv("Speed_Dating_Data.csv", encoding="cp1252")
+df = load_data("Speed_Dating_Data.csv")
 
-st.dataframe(df)
+st.dataframe(df.head())
+infor = df.info()
+st.dataframe(infor)
+
+st.dataframe(df.describe())
+
+df_num = df.select_dtypes(exclude="object")
+st.dataframe(df_num.columns)
+
+df_catagories = df.select_dtypes(include="object")
+st.dataframe(df_catagories.columns)
