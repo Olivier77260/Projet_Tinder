4be77c9f3c259645a0f6db3ad3d9ee688769ff28
Olivier Evrard <olivier.evrard77@gmail.com>
2024-08-06 21:55:49 +0200

    V 06.08.5

--- function.py
+++ function.py
@@ -4,13 +4,11 @@ import pandas as pd
 if 'del_from' not in st.session_state:
     st.session_state.del_from = False
 
-@st.cache_data
+# @st.cache_data
 def load_data(del_from):
     df = pd.read_csv("Speed_Dating_Data.csv", encoding="cp1252")
     if del_from:
         df = df.dropna(subset=['from', 'race'])
     return df
-if st.session_state.del_from:
-    load_data.clear()
 
 df = load_data(st.session_state.del_from)
