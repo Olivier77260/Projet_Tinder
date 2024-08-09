54f5d2e992dee484edc79512aa98f5f124668a5c
Olivier Evrard <50024014+Olivier77260@users.noreply.github.com>
2024-08-09 12:04:15 +0200

    Version 09.08.1

--- app.py
+++ app.py
@@ -21,7 +21,10 @@ def main():
     st.session_state.dfTrue = load_data_True()
     st.session_state.dfFalse = load_data_False() 
 
-    preambule = st.Page("feuillets/preambule.py", title="Projet", icon=":material/engineering:", default=True)
+    preambule = st.Page("feuillets/preambule.py", title="Préambule", icon=":material/engineering:", default=True)
+    Projet = st.Page("feuillets/projet.py", title="Projet", icon=":material/engineering:", default=True)
+    Objectif = st.Page("feuillets/objectif.py", title="Objectif", icon=":material/engineering:", default=True)
+    portee = st.Page("feuillets/portee.py", title="Portée du projet", icon=":material/engineering:", default=True)
 
     donnees = st.Page("feuillets/donnees.py", title="Données", icon=":material/database:")
 
@@ -39,7 +42,7 @@ def main():
     premier_rdv = st.Page("feuillets/first_rdv.py", title="Premier rendez-vous", icon=":material/diversity_3:")
 
     pages = {
-        "🏠 Préambule": [preambule,],
+        "🏠 Préambule": [preambule, Projet, Objectif, portee,],
         "📊 Données fournies": [donnees,],
         "🛠️ Analyse des données": [analyse,],
         "🌍 Profil des participants": [physique, social,],
